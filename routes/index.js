fa3b513acbb0b8396b39e734ff609e7cf6ebfc60
mapleLeafPlum <649980210@qq.com>
2019-12-20 17:36:49 +0800

    zxZx

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+let indexService=require('../service/indexService');
+
+router.get('/', function (req, res, next) {
+  res.send("hellow word");
+});
+
+router.post('/login', function (req, res, next) {
+    console.log(req.query);
+    res.json(req.query);
+});
+
+router.get('/sys/menu/nav', function (req, res, next) {
+  console.log(req.query);
+  indexService.getMenu(req.query,function (data) {
+    let result={
+      menuList:data,
+      authorities:[]
+    };
+    res.json(result);
+  });
+});
+
+router.get('/sys/user/info', function (req, res, next) {
+  console.log(req.query);
+  indexService.getInfo(req.query,function (data) {
+    res.json(data);
+  });
+});
+
+
+router.get('/captcha.jpg', function (req, res, next) {
+  console.log(req.query);
+  res.json(req.query);
+});
+
+
+
+
+
+
+router.post('/proxyApi/login', function (req, res, next) {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+router.get('/proxyApi/captcha.jpg', function (req, res, next) {
+    console.log(req.query);
+    res.json(req.query);
+});
+
+router.get('/proxyApi/sys/menu/nav', function (req, res, next) {
+  console.log(req.query);
+  res.json(req.query);
 });
 
 module.exports = router;
